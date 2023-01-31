95ae4eff104148ad2c5874cac9089eab3671bb3d
ncyborgse <nils.ml.borg@gmail.com>
2023-01-31 13:17:25 +0100

    test

---
+++ Lab2/GccApplication1/main.c
@@ -0,0 +1,18 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 2023-01-31 13:15:28
+ * Author : Nils
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
