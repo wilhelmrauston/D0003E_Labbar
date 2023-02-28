01cc279fdae5b0dcc97d839d14be295ec74ad183
ncyborgse <nils.ml.borg@gmail.com>
2023-02-28 15:04:52 +0100

    lab 5 start

---
+++ Lab5/GccApplication1/GccApplication1/Gui.h
@@ -0,0 +1,23 @@
+
+#ifndef GUI_H_
+#define GUI_H_
+
+#include "TinyTimber.h"
+#include <stdbool.h>
+#include <stdint.h>
+
+
+typedef struct GUI {
+  Object super;
+  uint8_t pos;
+} GUI;
+
+#define initGUI(pos) {initObject(), pos}
+
+void initLCD(GUI *self);
+
+void printAt(GUI *self, uint8_t num);
+
+void switchActive(GUI *self);
+
+#endif
---
+++ Lab5/GccApplication1/GccApplication1/State.c
@@ -0,0 +1,18 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 2023-02-28 13:25:58
+ * Author : Nils
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
